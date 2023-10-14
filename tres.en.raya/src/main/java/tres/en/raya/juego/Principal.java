dc2eea6ae16208d4720826816957d6793f660efd
Daniel1998-cenec <danielcesar1998@hotmail.com>
2023-10-14 12:45:28 +0200

    listo ejercicio 3 en rayas
    .

--- tres.en.raya/src/main/java/tres/en/raya/juego/Principal.java
+++ tres.en.raya/src/main/java/tres/en/raya/juego/Principal.java
@@ -3,17 +3,19 @@ package tres.en.raya.juego;
 public class Principal {
 
 	public static void main(String[] args) {
-		String tablero[]= {"_","_","_","_","_","_","_","_","_"};
-		byte tirada=0;
-		Funciones.mostrarTablero(tablero);
-		Funciones.juegaJugador("X", tablero);
-		
+		String tablero[] = { "_", "_", "_", "_", "_", "_", "_", "_", "_" };
+		byte tirada = 0;
+		boolean alguienGano=false;
 		do {
-		Funciones.mostrarTablero(tablero);
-		String ficha = tirada%2== 0 ?"X":"O";
-		Funciones.juegaJugador(ficha, tablero);
-		tirada++;
-		}while (true);
+			Funciones.mostrarTablero(tablero);
+			String ficha = tirada % 2 == 0 ? "X" : "O";
+			Funciones.juegaJugador(ficha, tablero);
+			alguienGano=Funciones.ganadorPartida(tablero);
+			if(alguienGano) {
+				System.out.println("Enhorabuena, jugador "+ficha);
+			}
+			tirada++;
+		} while (!alguienGano);
 	}
 
 }
